6913165fb486c6f0d612907b696da2ec7845212e
Biswaranjan <mbiswaranjan797@gmail.com>
2024-04-27 09:38:21 +0530

    Added the Router Api to check

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,11 @@
 import express, { Express } from "express";
+
 // import serverConfig from "./config/serverConfig";
+import apiRouter from "./routes";
 
 const app: Express = express();
 
+app.use('/api', apiRouter);
 app.listen(3000, () => {
   console.log(`Server started at *:3000`);
 });
---
+++ src/routes/v1/index.ts
@@ -0,0 +1,9 @@
+import express  from "express";
+
+const v1Router  = express.Router();
+
+v1Router.get('/', () => {
+
+});
+
+export default v1Router;

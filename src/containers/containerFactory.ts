8ee0884027a96d903abcc0694540c4e6b5eb1737
Biswaranjan Mohanty <mbiswaranjan797@gmail.com>
2024-11-18 21:53:54 +0530

    Added Code for Docker Container

---
+++ src/containers/containerFactory.ts
@@ -0,0 +1,20 @@
+import Docker from "dockerode";
+
+async function createContainer(imageName : string, cmdExecutable : string[]) {
+  const docker = new Docker();
+
+  const container = await docker.createContainer({
+    Image: imageName,
+    Cmd: cmdExecutable,
+    AttachStdin: true, // to enable input Stream 
+    AttachStdout: true, // to enable output Stream
+    AttachStderr: true, // To enable error Stream 
+    Tty: false,
+    OpenStdin: true,// Keep the  Input Stream Open even no Interaction is There
+  });
+
+  return container;
+
+}
+
+export default createContainer;
--- src/validators/createSubmissionValidator.ts
+++ src/validators/createSubmissionValidator.ts
@@ -1,22 +1,23 @@
-// We are using ZOD to validate the  data 
+// We are using ZOD to validate the  data
 
-// client => valifation middleware impl. by  zod => controller 
+// client => valifation middleware impl. by  zod => controller
 
 import { NextFunction, Request, Response } from "express";
 import { ZodSchema } from "zod";
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
-export const validateCreateSubmissionDto = (schema: ZodSchema<any>) => 
-    (req: Request, res: Response, next: NextFunction) => {
-        try {
-            schema.parse(req.body);
-            next();
-        } catch (error) {
-            console.error(error);
-            return res.status(400).json({
-                success : false,
-                message : 'Invalid Request Params Exist',
-                data : {},
-                error : error,
-            });
-        }
-    };
+export const validateCreateSubmissionDto =
+  (schema: ZodSchema<any>) =>
+  (req: Request, res: Response, next: NextFunction) => {
+    try {
+      schema.parse(req.body);
+      next();
+    } catch (error) {
+      console.error(error);
+      return res.status(400).json({
+        success: false,
+        message: "Invalid Request Params Exist",
+        data: {},
+        error: error,
+      });
+    }
+  };

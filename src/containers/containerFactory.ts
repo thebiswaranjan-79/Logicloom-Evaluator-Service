22d08ed586b97afcf8808fbe539fbd21e878b500
Biswaranjan Mohanty <mbiswaranjan797@gmail.com>
2025-01-09 12:07:56 +0530

    Added tle and mle handler

--- src/containers/containerFactory.ts
+++ src/containers/containerFactory.ts
@@ -1,20 +1,24 @@
 import Docker from "dockerode";
 
-async function createContainer(imageName : string, cmdExecutable : string[]) {
+async function createContainer(imageName: string, cmdExecutable: string[]) {
   const docker = new Docker();
 
   const container = await docker.createContainer({
     Image: imageName,
     Cmd: cmdExecutable,
-    AttachStdin: true, // to enable input Stream 
+    AttachStdin: true, // to enable input Stream
     AttachStdout: true, // to enable output Stream
-    AttachStderr: true, // To enable error Stream 
+    AttachStderr: true, // To enable error Stream
     Tty: false,
-    OpenStdin: true,// Keep the  Input Stream Open even no Interaction is There
+
+    HostConfig: {
+      Memory: 1024 * 1024 * 1024, // 2GB
+    },
+
+    OpenStdin: true, // Keep the  Input Stream Open even no Interaction is There
   });
 
   return container;
-
 }
 
 export default createContainer;
